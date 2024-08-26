11b7c10398745dc3d555171b899ab1b84c7067f2
Kamui2134 <ctorkey@gmail.com>
2024-08-27 01:21:37 +0300

    Footer is done

--- Gaming Site/assets/js/script.js
+++ Gaming Site/assets/js/script.js
@@ -6,7 +6,7 @@ const addEventOnElements = function (elements, eventType, callback) {
 	}
 }
 
-// HEADER
+// HEADER AND BACK TOP BTN
 
 const navbar = document.querySelector('[data-navbar]')
 const navbarToggler = document.querySelector('[data-navbar-toggler]')
@@ -29,12 +29,15 @@ addEventOnElements(navbarLinks, 'click', navbarClose)
 // header will be active(fixed) after scrolled down to 100px (i think, i can doing this in css)
 
 const header = document.querySelector('[data-header]')
+const backTopBtn = document.querySelector("[data-back-top-btn]")
 
 const activeElement = function () {
 	if (window.scrollY > 100) {
 		header.classList.add('active')
+    backTopBtn.classList.add('active')
 	} else {
 		header.classList.remove('active')
+    backTopBtn.classList.remove('active')
 	}
 }
 
@@ -50,3 +53,21 @@ const buttonHoverRipple = function (event) {
 }
 
 addEventOnElements(buttons, 'mousemove', buttonHoverRipple)
+
+// Scroll reveal
+
+const revealElements = document.querySelectorAll("[data-reveal]")
+
+const revealElementOnScroll = function () {
+  for(let i = 0, len = revealElements.length; i < len; i++) {
+    const isElementInsideWindow = revealElements[i].getBoundingClientRect().top < window.innerHeight / 1.1
+
+    if (isElementInsideWindow) {
+      revealElements[i].classList.add("revealed")
+    }
+  }
+}
+
+window.addEventListener("scroll", revealElementOnScroll)
+
+window.addEventListener("load", revealElementOnScroll)
