d3ef1f1fa195d9c36703d9ef31d616bc4fa8579f
Kamui2134 <ctorkey@gmail.com>
2024-08-26 18:57:03 +0300

    News section is done

--- Gaming Site/assets/js/script.js
+++ Gaming Site/assets/js/script.js
@@ -1,52 +1,52 @@
 'use strict'
 
 const addEventOnElements = function (elements, eventType, callback) {
-  for(let i = 0, len = elements.length; i < len; i++) {
-    elements[i].addEventListener(eventType, callback)
-  }
+	for (let i = 0, len = elements.length; i < len; i++) {
+		elements[i].addEventListener(eventType, callback)
+	}
 }
 
 // HEADER
 
-const navbar = document.querySelector("[data-navbar]")
-const navbarToggler = document.querySelector("[data-navbar-toggler]")
-const navbarLinks = document.querySelectorAll("[data-navbar-link]")
+const navbar = document.querySelector('[data-navbar]')
+const navbarToggler = document.querySelector('[data-navbar-toggler]')
+const navbarLinks = document.querySelectorAll('[data-navbar-link]')
 
 const toggleNavbar = function () {
-  navbar.classList.toggle("active")
-  this.classList.toggle("active")
+	navbar.classList.toggle('active')
+	this.classList.toggle('active')
 }
 
-navbarToggler.addEventListener("click", toggleNavbar)
+navbarToggler.addEventListener('click', toggleNavbar)
 
 const navbarClose = () => {
-  navbar.classList.remove("active")
-  navbarToggler.classList.remove("active")
+	navbar.classList.remove('active')
+	navbarToggler.classList.remove('active')
 }
 
-addEventOnElements(navbarLinks, "click", navbarClose)
+addEventOnElements(navbarLinks, 'click', navbarClose)
 
 // header will be active(fixed) after scrolled down to 100px (i think, i can doing this in css)
 
-const header = document.querySelector("[data-header]")
+const header = document.querySelector('[data-header]')
 
 const activeElement = function () {
-  if(window.scrollY > 100) {
-    header.classList.add("active")
-  } else {
-    header.classList.remove("active")
-  }
+	if (window.scrollY > 100) {
+		header.classList.add('active')
+	} else {
+		header.classList.remove('active')
+	}
 }
 
-window.addEventListener("scroll", activeElement)
+window.addEventListener('scroll', activeElement)
 
 //Hero section
 
-const buttons = document.querySelectorAll("[data-btn]")
+const buttons = document.querySelectorAll('[data-btn]')
 
-const buttonHoverRipple = function(event) {
-  this.style.setProperty("--top", `${event.offsetY}px`)
-  this.style.setProperty("--left", `${event.offsetX}px`)
+const buttonHoverRipple = function (event) {
+	this.style.setProperty('--top', `${event.offsetY}px`)
+	this.style.setProperty('--left', `${event.offsetX}px`)
 }
 
-addEventOnElements(buttons, "mousemove", buttonHoverRipple)
+addEventOnElements(buttons, 'mousemove', buttonHoverRipple)
