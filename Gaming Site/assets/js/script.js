fac936f6a81ac0a72235c2aaf35d0e770d7bc56d
Kamui2134 <ctorkey@gmail.com>
2024-08-29 01:27:20 +0300

    Site is done

--- Gaming Site/assets/js/script.js
+++ Gaming Site/assets/js/script.js
@@ -29,15 +29,15 @@ addEventOnElements(navbarLinks, 'click', navbarClose)
 // header will be active(fixed) after scrolled down to 100px (i think, i can doing this in css)
 
 const header = document.querySelector('[data-header]')
-const backTopBtn = document.querySelector("[data-back-top-btn]")
+const backTopBtn = document.querySelector('[data-back-top-btn]')
 
 const activeElement = function () {
 	if (window.scrollY > 100) {
 		header.classList.add('active')
-    backTopBtn.classList.add('active')
+		backTopBtn.classList.add('active')
 	} else {
 		header.classList.remove('active')
-    backTopBtn.classList.remove('active')
+		backTopBtn.classList.remove('active')
 	}
 }
 
@@ -56,18 +56,42 @@ addEventOnElements(buttons, 'mousemove', buttonHoverRipple)
 
 // Scroll reveal
 
-const revealElements = document.querySelectorAll("[data-reveal]")
+const revealElements = document.querySelectorAll('[data-reveal]')
 
 const revealElementOnScroll = function () {
-  for(let i = 0, len = revealElements.length; i < len; i++) {
-    const isElementInsideWindow = revealElements[i].getBoundingClientRect().top < window.innerHeight / 1.1
+	for (let i = 0, len = revealElements.length; i < len; i++) {
+		const isElementInsideWindow =
+			revealElements[i].getBoundingClientRect().top < window.innerHeight / 1.1
 
-    if (isElementInsideWindow) {
-      revealElements[i].classList.add("revealed")
-    }
-  }
+		if (isElementInsideWindow) {
+			revealElements[i].classList.add('revealed')
+		}
+	}
 }
 
-window.addEventListener("scroll", revealElementOnScroll)
+window.addEventListener('scroll', revealElementOnScroll)
 
-window.addEventListener("load", revealElementOnScroll)
+window.addEventListener('load', revealElementOnScroll)
+
+// Custom Cursor
+
+const cursor = document.querySelector('[data-cursor]')
+const hoverElements = [
+	...document.querySelectorAll("a"),
+	...document.querySelectorAll("button"),
+]
+
+const cursorMove = function (event) {
+	cursor.style.top = `${event.clientY}px`
+	cursor.style.left = `${event.clientX}px`
+}
+
+window.addEventListener('mousemove', cursorMove)
+
+addEventOnElements(hoverElements, 'mouseover', function () {
+	cursor.classList.add("hovered")
+})
+
+addEventOnElements(hoverElements, 'mouseout', function () {
+	cursor.classList.remove("hovered")
+})
