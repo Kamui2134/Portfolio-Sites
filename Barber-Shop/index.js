848c6bbce01f53ce913cf9af8ecbd8eccbc4bcdf
Kamui2134 <ctorkey@gmail.com>
2024-09-02 11:56:56 +0300

    BETA COMMIT)

--- Barber-Shop/index.js
+++ Barber-Shop/index.js
@@ -4,6 +4,6 @@ const text = document.querySelector('.circle-text p')
 text.innerHTML = text.innerText
 	.split('')
 	.map(
-		(char, i) => `<span style='transform:rotate(${i * 8.3}deg)'>${char}</span>`
+		(char, i) => `<span style='transform:rotate(${i * 12}deg)'>${char}</span>`
 	)
 	.join('')
